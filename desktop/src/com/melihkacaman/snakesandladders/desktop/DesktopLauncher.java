165e952f3f81e22630f0116706005ae48ade91f8
Melih Kaçaman <mkacamann@gmail.com>
2021-03-29 12:43:47 +0300

    initial commit

---
+++ desktop/src/com/melihkacaman/snakesandladders/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.melihkacaman.snakesandladders.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.melihkacaman.snakesandladders.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
